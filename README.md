38246012a82fc0200e5504498acf5ab88e280078
Stanislav Pugachev <129574544+stanislav-pugachev@users.noreply.github.com>
2023-04-06 05:40:14 +0100

    Первоначальные файлы для проекта MakeCode

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Открыть эту страницу в [https://stanislav-pugachev.github.io/man/](https://stanislav-pugachev.github.io/man/)
+
+## Использовать это расширение
+
+Этот репозиторий может быть добавлен в MakeCode как **расширение**.
+
+* открыть [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* нажмите на **Новый проект**
+* нажмите **Расширения** в меню-шестерёнке
+* найдите **https://github.com/stanislav-pugachev/man** и импортируйте
+
+## Править этот проект ![Значок статуса сборки](https://github.com/stanislav-pugachev/man/workflows/MakeCode/badge.svg)
+
+Редактировать этот репозиторий в MakeCode.
+
+* открыть [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* нажмите на **Импорт**, затем **Импорт URL**
+* вставьте **https://github.com/stanislav-pugachev/man** и нажмите Импорт
+
+## Предварительный просмотр блоков
+
+Это изображение показывает блоки кода из последнего коммита в главной ветке.
+Обновление этого изображения может занять несколько минут.
+
+![Отрисовка в виде блоков](https://github.com/stanislav-pugachev/man/raw/master/.github/makecode/blocks.png)
+
+#### Метаданные (используются для поиска, рендеринга)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,122 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . # . # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . # . .
+        # # # # #
+        . . # . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . #
+        # # # # #
+        # . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . # . # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        # # # # #
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . # . # .
+        `)
+})
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    . . # . .
+    . # . # .
+    `)
+basic.forever(function () {
+	
+})
